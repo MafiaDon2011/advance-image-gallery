9abf48031abb52ebcec78252edabdf08fa1d1ef0
MafiaDon2011 <wajidhussain1@live.com>
2023-01-07 14:42:28 +0000

    Added code to make image no disappear when transitioning to another image

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const back = document.getElementById('back')
 const forward = document.getElementById('forward')
 const name = document.querySelector('.name')
 const content = document.querySelector('.content')
+const nextImg = document.getElementById("next-img")
 
 let activeIndex = 0
 
@@ -19,9 +20,12 @@ back.onclick = () => {
     if (activeIndex !== 0) {
         activeIndex = activeIndex - 1
         Slide(activeIndex)
+        // next img src
+        nextImg.src = imgs[activeIndex + 1].src
     } else {
         activeIndex = imgs.length - 1
         Slide(activeIndex)
+        nextImg.src = imgs[0].src
     }
 }
 
@@ -29,8 +33,10 @@ forward.onclick = () => {
     if(imgs.length - 1 > activeIndex) {
         activeIndex = activeIndex + 1
         Slide(activeIndex)
+        nextImg.src = imgs[activeIndex - 1].src
     } else {
         activeIndex = 0
         Slide(activeIndex)
+        nextImg.src = imgs[imgs.length - 1].src
     }
 }
